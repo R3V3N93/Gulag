fc6dd0480114a38f8be68cad1057df60892b825b
R3V3N93 <123841546+R3V3N93@users.noreply.github.com>
2024-12-19 03:24:43 +0900

    xd

--- ZandroDev3.2-241030-1744windows-x86_64/compile.bat
+++ ZandroDev3.2-241030-1744windows-x86_64/compile.bat
@@ -1,4 +1,5 @@
 @echo off
-bcc\zt-bcc.exe gulag\acs_source\gulag.acs gulag\acs\gulag.o -acc-err
-
+bcc\zt-bcc.exe gulag\acs_source\gulag.acs gulag\acs\gulag.o
+timeout /t 1
+powershell Compress-Archive -Path "gulag" -DestinationPath "gulag.zip"
 pause
